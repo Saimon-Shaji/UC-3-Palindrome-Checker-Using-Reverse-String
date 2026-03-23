876da586ae69f76d0130dd24fd3fb4c192a49e06
Saimon-Shaji <ss0175@srmist.edu.in>
2026-03-23 11:03:59 +0530

    Updated input test case

--- src/UseCase6PalindromeCheckerApp.java
+++ src/UseCase6PalindromeCheckerApp.java
@@ -4,7 +4,7 @@ public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "lol";
 
         Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
